0ff7f6d7c80cebdbf07c2891ae1a41cd4245d4f9
1051521874@qq.com <Wife19951118>
2019-10-07 22:07:12 +0800

    init commit

---
+++ src/main/java/com/github/tom/selfregulation/SpringBootSelfRegulationDappApplication.java
@@ -0,0 +1,13 @@
+package com.github.tom.selfregulation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSelfRegulationDappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSelfRegulationDappApplication.class, args);
+    }
+
+}
